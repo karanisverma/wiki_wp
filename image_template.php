d9c05780b3ddc270724d13913a5b560d3e6f17af
Karan Verma <karan.verma@hindustanuniv.ac.in>
2016-02-17 00:27:02 +0530

    formatted properly

--- image_template.php
+++ image_template.php
@@ -4,23 +4,24 @@ Template Name: Pic Gallery Page
 */
 ?>
 <?php 
-// quering all the post from wordpress
+/* Argument to query all the post from wordpress*/
 $args = array('post_type' => 'post');
+/* Wordpress Query*/
 $the_query = new WP_Query( $args ); ?>
 <?php if ( $the_query->have_posts() ) : ?>
-	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-<?php
-	$post_id =get_the_ID();
-    if ( get_post_gallery() ) :
-    		$gallery = get_post_gallery( $post_id, false );
-            /* Loop through all the image and output them one by one */
-            foreach( $gallery['src'] as $src ) : ?><img src="<?php echo $src; ?>" class="my-custom-class" alt="Gallery image" />
-                 <?php
-            endforeach;
-        endif;
-    endwhile;
-?>
-	<?php wp_reset_postdata(); ?>
+    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+            <?php
+                $post_id =get_the_ID();
+                if ( get_post_gallery() ) :
+                        $gallery = get_post_gallery( $post_id, false );
+                        /* Loop through all the image and output them one by one */
+                        foreach( $gallery['src'] as $src ) : ?><img src="<?php echo $src; ?>" class="img_class" alt="Gallery image" />
+                             <?php
+                        endforeach;
+                    endif;
+                endwhile;
+            ?>
+    <?php wp_reset_postdata(); ?>
 <?php else : ?>
-	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <p><?php _e( 'Sorry, NO post found!' ); ?></p>
 <?php endif; ?>
